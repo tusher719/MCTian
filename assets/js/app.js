bddb03d8e25cdf53aa5be871fc46d5e4d0ce34c4
Tusher <tusher4572@gmail.com>
2023-04-03 18:40:53 +0600

    6th person added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,32 @@
-$('[data-countdown]').each(function () {
-    var $deadLine = new Date($(this).data('countdown')).getTime();
-
-    var $dataDays = $(this).children('[data-days]');
-    var $dataHours = $(this).children('[data-hours]');
-    var $dataMinuts = $(this).children('[data-minuts]');
-    var $dataSeconds = $(this).children('[data-seconds]');
-
-
-    var x = setInterval(function () {
-        var now = new Date().getTime();
-        var t = $deadLine - now;
-
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(t % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-        var minuts = Math.floor(t % (1000 * 60 * 60) / (1000 * 60));
-        var seconds = Math.floor(t % (1000 * 60) / (1000));
-
-        $dataDays.html(days);
-        $dataHours.html(hours);
-        $dataMinuts.html(minuts);
-        $dataSeconds.html(seconds);
+$('[data-countdown]').each(function () {                    
+    var $dataDays = $(this).find("[data-days]");
+    var $dataHours = $(this).find("[data-hours]");
+    var $dataMinutes = $(this).find("[data-minutes]");
+    var $dataSeconds = $(this).find("[data-seconds]");
+  
+    var birthdate = new Date($(this).data("countdown"));
+    birthdate.setFullYear(new Date().getFullYear());
+    var now = new Date().getTime();
+    if (birthdate.getTime() < now) {
+      birthdate.setFullYear(new Date().getFullYear() + 1);
+    }
+    var countdown = setInterval(function () {
+      var now = new Date().getTime();
+      var t = birthdate.getTime() - now;
+  
+      var days = Math.floor(t / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((t % (1000 * 60)) / 1000);
+  
+      $dataDays.html(days);
+      $dataHours.html(hours);
+      $dataMinutes.html(minutes);
+      $dataSeconds.html(seconds);
+  
+      if (t < 0) {
+        clearInterval(countdown);
+      }
     }, 1000);
-});
+  });
+  
